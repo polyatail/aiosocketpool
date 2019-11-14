1301458b893cb4dbafcbf01eca4036483c07d4ee
polyatail <andrewscz@gmail.com>
2019-11-14 13:26:32 -0800

    Update dependencies

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setup(
     version=__version__,
     packages=find_packages(exclude=["*test*"]),
     install_requires=[
-        "async_timeout",
+        "async_timeout>=3.0.1",
     ],
     extras_require={
         "testing": [
             "black>=19.3b0",
             "flake8>=3.7.8",
+            "piptools>=4.2.0",
             "pydocstyle>=4.0.0",
             "pytest>=5.1.0",
+            "pytest-asyncio>=0.10.0",
         ]
     },
 )
