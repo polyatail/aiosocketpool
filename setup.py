04500b172978da7d1871a4f50be660b273cb03f7
polyatail <andrewscz@gmail.com>
2019-12-12 16:53:29 -0800

    Add CircleCI configuration

--- setup.py
+++ setup.py
@@ -7,14 +7,12 @@ setup(
     name="aio_socketpool",
     version=__version__,
     packages=find_packages(exclude=["*test*"]),
-    install_requires=[
-        "async_timeout>=3.0.1",
-    ],
+    install_requires=["async_timeout>=3.0.1"],
     extras_require={
         "testing": [
             "black>=19.3b0",
             "flake8>=3.7.8",
-            "piptools>=4.2.0",
+            "pip-tools>=4.2.0",
             "pydocstyle>=4.0.0",
             "pytest>=5.1.0",
             "pytest-asyncio>=0.10.0",
