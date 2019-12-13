8f5a2f4e6b4203fdfb6c8356d836c0577e8549a4
polyatail <andrewscz@gmail.com>
2019-12-13 11:27:05 -0800

    Use python typing and improve docstrings

--- aio_socketpool/__init__.py
+++ aio_socketpool/__init__.py
@@ -6,6 +6,7 @@ import platform
 import select
 import socket
 import time
+from typing import Optional
 import weakref
 
 import async_timeout
@@ -22,7 +23,18 @@ def can_use_kqueue():
     return True
 
 
-def is_connected(sock):
+def is_connected(sock: socket.socket):
+    """Determine if a `socket.socket` object is actually connected.
+
+    Notes
+    -----
+    This method tries to determine whether a socket is connected in a platform-agnostic way. It is
+    copied verbatim from `https://github.com/benoitc/socketpool/blob/master/socketpool/util.py`.
+
+    Returns
+    -------
+    `bool`
+    """
     try:
         fno = sock.fileno()
     except socket.error as e:
@@ -84,6 +96,198 @@ def is_connected(sock):
     return False
 
 
+class SocketPoolException(Exception):
+    pass
+
+
+class BaseConnector(object):
+    def __init__(self, pool=None):
+        self._connected = False
+        self._connected_at = -1
+
+        # every connector must accept pool as a kwarg
+        self._pool = pool
+
+    def __del__(self):
+        self.release()
+
+    def release(self):
+        if self._pool is not None:
+            if self._connected:
+                self._pool.release_connection(self)
+            else:
+                self._pool = None
+
+    async def connect(self) -> bool:
+        self._connected = True
+        self._connected_at = time.time()
+
+        return True
+
+    def get_lifetime(self) -> float:
+        return self._connected_at
+
+    def invalidate(self):
+        self._connected = False
+        self._connected_at = -1
+
+    def matches(self, **match_options) -> bool:
+        raise NotImplementedError
+
+    def is_connected(self) -> bool:
+        raise NotImplementedError
+
+
+class AsyncTcpConnector(BaseConnector):
+    def __init__(self, host: str, port: int, pool=None, timeout: Optional[int] = None):
+        """Asyncio TCP socket connection factory.
+
+        Parameters
+        ----------
+        host : `str`
+        port : `int`
+        pool : `AsyncConnectionPool`
+        timeout : `int`
+            Time (in seconds) before giving up on connect, send, and receive operations.
+        """
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setblocking(0)
+
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+        super().__init__(pool=pool)
+
+    async def connect(self) -> bool:
+        """Establish a connection to the remote host.
+
+        Returns
+        -------
+        `True` if the connection was successfully established, raises if not.
+        """
+        if __debug__:
+            logging.debug(f"AsyncTcpConnector: new connection to {self.host}:{self.port}")
+
+        loop = asyncio.get_event_loop()
+
+        with async_timeout.timeout(self.timeout):
+            await loop.sock_connect(self._socket, (self.host, self.port))
+
+        if not is_connected(self._socket):
+            raise SocketPoolException(f"Connection to {self.host}:{self.port} failed")
+
+        await super().connect()
+
+        return True
+
+    def matches(self, **match_options) -> bool:
+        """Determine if this connector matches the specified parameters.
+
+        Returns
+        -------
+        `True` if the given host and port match this connector, `False` otherwise.
+        """
+        target_host = match_options.get("host")
+        target_port = match_options.get("port")
+
+        return target_host == self.host and target_port == self.port
+
+    def is_connected(self) -> bool:
+        """Determine if the socket held by this connector is actually connected.
+
+        Uses the platform-agnostic `is_connected` method defined above to determine if the socket
+        is actually connected, and hasn't, e.g., timed out.
+
+        Returns
+        -------
+        `bool`
+        """
+        if self._connected:
+            return is_connected(self._socket)
+
+        return False
+
+    def invalidate(self):
+        """Close the socket held by this connector."""
+        self._socket.close()
+        super().invalidate()
+
+    async def sendall(self, data: bytes):
+        """Send all given data to the remote host.
+
+        See the documentation for `socket.socket.sendall` for more information.
+
+        Parameters
+        ----------
+        data : `bytes`
+
+        Returns
+        -------
+        `None` on success
+        """
+        loop = asyncio.get_event_loop()
+
+        with async_timeout.timeout(self.timeout):
+            return await loop.sock_sendall(self._socket, data)
+
+    async def recv(self, size: Optional[int] = 1024) -> bytes:
+        """Receive some data from the remote host.
+
+        See the documentation for `socket.socket.recv` for more information.
+
+        Parameters
+        ----------
+        size : `int`
+            The maximum amount of data to receive.
+
+        Returns
+        -------
+        `bytes`
+        """
+        loop = asyncio.get_event_loop()
+
+        with async_timeout.timeout(self.timeout):
+            return await loop.sock_recv(self._socket, size)
+
+    async def recv_exactly(self, size: int) -> bytes:
+        """Receive an exact amount of data from the remote host.
+
+        Parameters
+        ----------
+        size : `int`
+            The exact amount of data to receive before returning.
+
+        Notes
+        -----
+        Unlike `recv`, this method will continuously receive data from the host and add it to a
+        buffer until it contains the desired amount of data. This method will receive no more (and
+        no less) than `size` bytes of data before returning.
+
+        Returns
+        -------
+        `bytes`
+        """
+        data = b""
+
+        while True:
+            data_remaining = size - len(data)
+            buf = await self.recv(4096 if data_remaining > 4096 else data_remaining)
+
+            if not buf:
+                break
+
+            data += buf
+
+            if len(data) >= size:
+                break
+
+        if len(data) != size:
+            raise SocketPoolException(f"Expected {size}, got {len(data)}")
+
+        return data
+
+
 class IterablePriorityQueue(asyncio.PriorityQueue):
     def __iter__(self):
         return self
@@ -102,10 +306,10 @@ class IterablePriorityQueue(asyncio.PriorityQueue):
 class AsyncConnectionPool(object):
     """Create a pool of connections, anticipating use in an async/await context.
 
-    We will maintain a pool of `max_size` connections for approximately `max_lifetime`. New
-    connections will be created as needed. We will not block if the user requests more than
-    `max_size` concurrent connections: that is only a limit on the number of connections we'll keep
-    in the pool, not a limit on concurrency.
+    Maintains a pool of `max_size` connections for approximately `max_lifetime`. New connections are
+    created as needed. Does not block if the user requests more than `max_size` concurrent
+    connections: `max_size` is only a limit on the number of connections we'll keep in the pool, not
+    a limit on concurrency.
 
     Parameters
     ----------
@@ -116,7 +320,7 @@ class AsyncConnectionPool(object):
         Maximum number of connections to keep in the pool.
     options : `dict`
         Keyword arguments to be passed to the connection factory.
-    reap_connection : `bool`
+    reap_connections : `bool`
         If True, start a background task to destroy connections that are too old.
     reap_delay : `int`
         Time (in seconds) between loops of connection reaper.
@@ -124,12 +328,12 @@ class AsyncConnectionPool(object):
 
     def __init__(
         self,
-        factory,
-        max_lifetime=600.0,
-        max_size=10,
+        factory: BaseConnector,
+        max_lifetime: int = 600,
+        max_size: int = 10,
         options=None,
-        reap_connections=True,
-        reap_delay=1,
+        reap_connections: bool = True,
+        reap_delay: int = 1,
     ):
         self.factory = factory
         self.max_lifetime = max_lifetime
@@ -148,7 +352,7 @@ class AsyncConnectionPool(object):
     def __del__(self):
         self.stop_reaper()
 
-    def too_old(self, conn):
+    def too_old(self, conn: BaseConnector) -> bool:
         return time.time() - conn.get_lifetime() > self.max_lifetime
 
     def start_reaper(self):
@@ -182,7 +386,7 @@ class AsyncConnectionPool(object):
             if current_pool_size == 0:
                 break
 
-    def _reap_connection(self, conn):
+    def _reap_connection(self, conn: BaseConnector):
         if conn.is_connected():
             conn.invalidate()
 
@@ -195,10 +399,10 @@ class AsyncConnectionPool(object):
             pass
 
     @property
-    def size(self):
+    def size(self) -> int:
         return self.pool.qsize()
 
-    def reap_pool(self):
+    def reap_pool(self) -> int:
         """Reap all idle connectors currently in the pool.
 
         Notes
@@ -218,7 +422,7 @@ class AsyncConnectionPool(object):
 
         return current_pool_size
 
-    def reap_all(self):
+    def reap_all(self) -> int:
         """Reap all connectors, including those active and idle.
 
         Returns
@@ -232,12 +436,16 @@ class AsyncConnectionPool(object):
 
         return len(connections)
 
-    def release_connection(self, conn):
+    def release_connection(self, conn: BaseConnector):
         """Place a connector back in the pool or reap it if we cannot.
+
+        Parameters
+        ----------
+        conn : `BaseConnector`
 
         Notes
         -----
-        If a connector is too old, or we are going to exceed max_size, drop it.
+        If a connector is too old, or we are going to exceed `max_size`, drop it.
         """
         if self._reaper_task is not None:
             self.ensure_reaper_started()
@@ -255,8 +463,19 @@ class AsyncConnectionPool(object):
         else:
             self._reap_connection(conn)
 
-    async def get(self, **options):
-        """Get a connector matching the given options, creating one if necessary."""
+    async def get(self, **options) -> BaseConnector:
+        """Get a connector matching the given options, creating one if necessary.
+
+        Notes
+        -----
+        This method will call `BaseConnector.matches(**options)` on every connector in the pool to
+        determine if a connection already exists matching the given parameters. If one does not, a
+        new `BaseConnector` will be created with the given `options` as keyword arguments.
+
+        Returns
+        -------
+        `BaseConnector`
+        """
         options.update(self.options)
         options["pool"] = self
 
@@ -315,114 +534,13 @@ class AsyncConnectionPool(object):
 
         # the connection factory should raise if the connection fails, so we should never get to
         # this point. we'll raise anyway just to make sure.
-        raise Exception("Connection factory failed to connect and did not raise")
+        raise SocketPoolException("Connection factory failed to connect and did not raise")
 
     @contextlib.asynccontextmanager
-    async def connection(self, **options):
+    async def connection(self, **options) -> BaseConnector:
         conn = await self.get(**options)
 
         try:
             yield conn
         finally:
             self.release_connection(conn)
-
-
-class AsyncTcpConnector(object):
-    def __init__(self, host, port, pool=None, timeout=None):
-        """Asyncio TCP socket connection factory.
-
-        Parameters
-        ----------
-        host : `str`
-        port : `int`
-        pool : `AsyncConnectionPool`
-        timeout : `int`
-            Time (in seconds) before giving up on connect, send, and receive operations.
-        """
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.setblocking(0)
-
-        self.host = host
-        self.port = port
-        self.timeout = timeout
-
-        self._connected = False
-        self._connected_at = -1
-        self._pool = pool
-
-    def __del__(self):
-        self.release()
-
-    async def connect(self):
-        if __debug__:
-            logging.debug(f"AsyncTcpConnector: new connection to {self.host}:{self.port}")
-
-        loop = asyncio.get_event_loop()
-
-        with async_timeout.timeout(self.timeout):
-            result = await loop.sock_connect(self._socket, (self.host, self.port))
-
-        self._connected = True
-        self._connected_at = time.time()
-
-        return result
-
-    def matches(self, **match_options):
-        """Return True if our socket is connected to the specified host and port."""
-        target_host = match_options.get("host")
-        target_port = match_options.get("port")
-
-        return target_host == self.host and target_port == self.port
-
-    def is_connected(self):
-        if self._connected:
-            return is_connected(self._socket)
-
-        return False
-
-    def get_lifetime(self):
-        return self._connected_at
-
-    def invalidate(self):
-        self._socket.close()
-        self._connected = False
-        self._connected_at = -1
-
-    def release(self):
-        if self._pool is not None:
-            if self._connected:
-                self._pool.release_connection(self)
-            else:
-                self._pool = None
-
-    async def sendall(self, *args):
-        loop = asyncio.get_event_loop()
-
-        with async_timeout.timeout(self.timeout):
-            return await loop.sock_sendall(self._socket, *args)
-
-    async def recv(self, size=1024):
-        loop = asyncio.get_event_loop()
-
-        with async_timeout.timeout(self.timeout):
-            return await loop.sock_recv(self._socket, size)
-
-    async def recv_exactly(self, size):
-        data = b""
-
-        while True:
-            data_remaining = size - len(data)
-            buf = await self.recv(4096 if data_remaining > 4096 else data_remaining)
-
-            if not buf:
-                break
-
-            data += buf
-
-            if len(data) >= size:
-                break
-
-        if len(data) != size:
-            raise Exception(f"Expected {size}, got {len(data)}")
-
-        return data
